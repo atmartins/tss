c41c63339f0b287225febae96aaa743748daf4da
Aaron Martins <amartins@uoregon.edu>
2013-10-08 13:50:37 -0700

    added thumbs

--- js/directives.js
+++ js/directives.js
@@ -1,10 +1,19 @@
 'use strict';
 
 /* Directives */
-angular.module('tssDirectives', ['ngResource'])
+/*angular.module('tssDirectives', ['ngResource'])
   .directive('superman', function() {    
     return {
      	restrict:"E",
      	template:"<div>Here I am to save the day</div>"
+    }
+  });
+*/
+angular.module('tssDirectives', ['ngResource'])
+  .directive('superman', function() {    
+    return function(scope, element){
+    	element.bind("mouseenter",function(){
+    		console.log(scope.template.name + " mouse entered element");
+    	})
     }
   });
